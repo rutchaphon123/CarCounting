4137b4313ec36d3e24532b53484d7e2e96d170ea
rutchaphon123 <rutchaphon.far@gmail.com>
2024-07-05 12:01:39 +0700

    fix run.bat

--- run.bat
+++ run.bat
@@ -3,12 +3,25 @@
 rem เปลี่ยนไปยังไดเรกทอรี่ที่เก็บไฟล์ batch file
 cd %~dp0
 
-rem เรียกใช้ active.bat เพื่อ activate Python environment
-call env\Scripts\activate.bat
+rem แสดงตำแหน่งปัจจุบันหลังเปลี่ยนไดเรกทอรี่
+echo Current directory after change: %CD%
 
-rem รอ 1 วินาทีหลังจาก activate environment
-timeout /t 1
+rem ตรวจสอบว่ามี virtual environment อยู่แล้วหรือไม่
+if not exist "env" (
+    echo Creating virtual environment...
+    python -m venv env
+) else (
+    echo Virtual environment already exists.
+)
 
+rem Activate virtual environment
+call /env/Scripts/activate.bat
+
+rem ติดตั้ง dependencies จาก requirement.txt
+echo Installing requirement...
+pip install -r requirement.txt
+
+rem รัน Python script
 python main.py
 
 rem บันทึกค่า exit code หลังรัน Python script
@@ -16,5 +29,5 @@ set exitcode=%errorlevel%
 
 rem Deactivate Python environment (if script exited with error)
 if %exitcode% neq 0 (
-  call env\Scripts\deactive.bat
+    call /env/Scripts/deactivate.bat
 )
