bdf80fdcc809255ad0fa4057d11449ee37d38634
rutchaphon123 <rutchaphon.far@gmail.com>
2024-07-05 12:18:21 +0700

    fix run.bat

--- run.bat
+++ run.bat
@@ -5,12 +5,7 @@ cd %~dp0
 
 rem แสดงตำแหน่งปัจจุบันหลังเปลี่ยนไดเรกทอรี่
 echo Current directory after change: %CD%
-<<<<<<< HEAD
-=======
 
-rem เรียกใช้ active.bat เพื่อ activate Python environment
-call ./env/Scripts/activate.bat"
->>>>>>> af40671d959b6a74a0e6e0c178b9a9a614edc419
 
 rem ตรวจสอบว่ามี virtual environment อยู่แล้วหรือไม่
 if not exist "env" (
@@ -21,7 +16,7 @@ if not exist "env" (
 )
 
 rem Activate virtual environment
-call /env/Scripts/activate.bat
+call ./env/Scripts/activate.bat
 
 rem ติดตั้ง dependencies จาก requirement.txt
 echo Installing requirement...
@@ -35,10 +30,5 @@ set exitcode=%errorlevel%
 
 rem Deactivate Python environment (if script exited with error)
 if %exitcode% neq 0 (
-<<<<<<< HEAD
-    call /env/Scripts/deactivate.bat
+    call ./env/Scripts/deactivate.bat
 )
-=======
-  call ./env/Scripts/deactivate.bat"
-)
->>>>>>> af40671d959b6a74a0e6e0c178b9a9a614edc419
